883e22c473ae43f2888067c878d4844628a8a031
rahul gupta <rahulguptaslg20@gmail.com>
2023-10-01 08:09:55 +0530

    Profile UI created

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -2,9 +2,9 @@ import {useSelector} from 'react-redux'
 import { Outlet , Navigate} from "react-router-dom"
 
 function PrivateRoute() {
-    const currentUser = useSelector((state) => state.user)
-    console.log(currentUser.currentUser)
-  return currentUser.currentUser ? <Outlet/> : <Navigate to = '/sign-in'/>
+    const currentUser = useSelector((state) => state.user.currentUser)
+    console.log(currentUser)
+  return currentUser ? <Outlet/> : <Navigate to = '/sign-in'/>
 }
 
 export default PrivateRoute
