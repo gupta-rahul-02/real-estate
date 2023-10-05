52ae9cab8d245f1cb91af07a97a8d52c7ec399ef
rahul gupta <rahulguptaslg20@gmail.com>
2023-10-06 02:24:45 +0530

    Create listing ui added

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -17,9 +17,10 @@ import {
   signOutStart,
   signInFailure,
   signOutFailure,
-  signOutSuccess
+  signOutSuccess,
 } from "../redux/user/userSlice.js";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 function Profile() {
   const currentUser = useSelector((store) => store.user.currentUser);
@@ -93,11 +94,11 @@ function Profile() {
     }
   };
 
-  const handleDeleteUser = async() => {
+  const handleDeleteUser = async () => {
     try {
       dispatch(deleteUserStart());
       const res = await fetch(`/api/user/delete/${currentUser._id}`, {
-        method: "DELETE"
+        method: "DELETE",
       });
       const data = await res.json();
       if (data.success === false) {
@@ -105,28 +106,25 @@ function Profile() {
         return;
       }
       dispatch(deleteUserSuccess(data));
-
     } catch (error) {
       dispatch(deleteUserFailure(error.message));
     }
   };
 
-  const handleSignOut = async() => {
+  const handleSignOut = async () => {
     try {
-      dispatch(signOutStart())
-      const res = await fetch('/api/auth/signout')
-      const data = await res.json()
-      if(data.success === false){
+      dispatch(signOutStart());
+      const res = await fetch("/api/auth/signout");
+      const data = await res.json();
+      if (data.success === false) {
         dispatch(signInFailure(data.message));
-        return
+        return;
       }
-      dispatch(signOutSuccess(data))
+      dispatch(signOutSuccess(data));
     } catch (error) {
       dispatch(signOutFailure(data.message));
-      
     }
-   
-  }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -185,6 +183,12 @@ function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link
+          to={"/create-listing"}
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+        >
+          Create List
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
@@ -193,13 +197,14 @@ function Profile() {
         >
           Delete account
         </span>
-        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          Sign out
+        </span>
       </div>
 
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
       <p className="text-green-700 mt-5">
         {updateSuccess ? "User is updated successfully !!!" : ""}
-        
       </p>
     </div>
   );
